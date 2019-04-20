9b62c366381d67239dd24f55a07b9b660126e3e7
assertivist <andy.halstead@gmail.com>
2019-04-19 23:04:46 -0600

    hello world

---
+++ main.js
@@ -0,0 +1,19 @@
+'use strict'
+const electron = require('electron')
+
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+app.on('ready', function() {
+	mainWindow = new BrowserWindow({
+		width:1024,
+		height:768
+	})
+
+	mainWindow.loadUrl(`file://${__dirname }/index.html`)
+	mainWindow.webContents.openDevTools()
+
+	mainWindow.on('closed', function() { mainWindow = null })
+});
---
+++ ports.js
@@ -0,0 +1,6 @@
+'use strict'
+const Elm = require('./elm.js')
+
+let container = document.getElementById('container')
+
+let pollard = Elm.Main.embed(container)
